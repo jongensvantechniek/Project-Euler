68e3a592d88a43152c310eaa09f2bd79cd86550d
Steven <steven@jongensvantechniek.nl>
2016-01-28 22:50:25 +0100

    Remove class definition from solution

--- problem1-swift/problem1.swift
+++ problem1-swift/problem1.swift
@@ -4,29 +4,27 @@
 
     Find the sum of all the multiples of 3 or 5 below 1000.
 */
-class Problem01 {
 
-    let MAX = 1000                              // Upperbound
-    let factors:Array<Int> = [3,5]              // Factors to check for multiples
+let MAX = 1000                              // Upperbound
+let factors:Array<Int> = [3,5]              // Factors to check for multiples
 
-    /*
-        Iterates through the given Natural numbers collection.
-        If an element of the Natural numbers collections is a multiple
-        of the given factors, add it to the current sum.
-    */
-    func findSum() {
-        var sumOfMultiples:Int = 0              // Initialize and set sum to 0
+/*
+    Iterates through the given Natural numbers collection.
+    If an element of the Natural numbers collections is a multiple
+    of the given factors, add it to the current sum.
+*/
 
-        for i in factors.minElement()!..<MAX {  // Iterate from lowerbound to upperbound.
-            for j in factors {                  // Iterate over given factors.
-                if i % j == 0 {                 // If current factor is a multiple of current element of the collection N,
-                    sumOfMultiples += i         // add the current element to the current sum.
-                    break
-                }
+func findSum() {
+    var sumOfMultiples:Int = 0              // Initialize and set sum to 0
+
+    for i in factors.minElement()!..<MAX {  // Iterate from lowerbound to upperbound.
+        for j in factors {                  // Iterate over given factors.
+            if i % j == 0 {                 // If current factor is a multiple of current element of the collection N,
+                sumOfMultiples += i         // add the current element to the current sum.
+                break
             }
         }
-
-        print(sumOfMultiples)                   // Return summated result of found factor multiples.
     }
 
+    print(sumOfMultiples)                   // Return summated result of found factor multiples.
 }
