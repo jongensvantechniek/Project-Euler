7f4d7bed4fbe61bc9e2f926954f871cefa445721
barry77 <barry@jongensvantechniek.nl>
2016-01-29 09:12:43 +0100

    corrected problem1 php no use of a function

--- problem1-php/problem1.php
+++ problem1-php/problem1.php
@@ -3,15 +3,12 @@
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
-function problem1()
-{
-	$total = 0; $until = 1000;
-	for ($i = 3; $i < $until; $i += 1) {
-		// Use mod to calculate if a number is divisible by itself, if so add it to the total sum
-		if ($i % 3 === 0 || $i % 5 === 0) {
-			$total += $i;
-		}
+
+$total = 0; $until = 1000;
+for ($i = 3; $i < $until; $i += 1) {
+	// Use mod to calculate if a number is divisible by itself, if so add it to the total sum
+	if ($i % 3 === 0 || $i % 5 === 0) {
+		$total += $i;
 	}
-	return $total;
 }
-echo problem1();
+echo $total;
